fd545c1f0dce9afaa8192b0932c0946eda1bff6c
Jacob Bullock <jacob.bullock@gmail.com>
2019-08-25 11:30:31 -0600

    init commit / starting position

---
+++ LootLogger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LootLogger
+//
+//  Created by jacob on 8/25/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, 360 iDev")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LootLogger/Model/Item.swift
@@ -0,0 +1,36 @@
+//
+//  Item.swift
+//  LootLogger
+//
+//  Created by jacob on 8/25/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name = ""
+    var serial = ""
+    var itemValue = ""
+    let id = UUID()
+    
+    init(name: String, serial: String, itemValue: String) {
+        self.name = name
+        self.serial = serial
+        self.itemValue = itemValue
+    }
+
+    static var random: Item {
+        let name = ["Banana", "Hat", "Sticker"].randomElement() ?? ""
+        let serial = String(UUID().uuidString.prefix(10))
+        let value = String((0..<100).randomElement() ?? 0)
+        return Item(name: name, serial: serial, itemValue: value)
+    }
+    
+    static var new: Item {
+        let name = "New Item"
+        let serial = String(UUID().uuidString.prefix(10))
+        let value = "0"
+        return Item(name: name, serial: serial, itemValue: value)
+    }
+}
